54cd2d93055b25d3086a524b76b59064d38b4ad0
craeden <wildtron10@gmail.com>
2013-11-24 03:28:29 +0800

    error hunt search.php

--- application/controller/search.php
+++ application/controller/search.php
@@ -18,7 +18,7 @@ class Search extends Kiel_Controller
 		$bangon = array();
 
 
-		$url = 'http://graph.facebook.com/666212400066213/photos?fields=name,tag,source,link&limit=1000';
+		$url = 'http://graph.facebook.com/666212400066213/photos?fields=name,tags,source,link&limit=1000';
 		$response = file_get_contents($url);
 		if($response){
 		$array = json_decode($response, true);
