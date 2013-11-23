c61169c39da4ab85cc18187a9691d4611249042e
craeden <wildtron10@gmail.com>
2013-11-24 03:24:11 +0800

    error hunt search.php

--- application/controller/search.php
+++ application/controller/search.php
@@ -24,7 +24,7 @@ class Search extends Kiel_Controller
 		$array = json_decode($response, true);
 		foreach($array as $p){
 				$name = "";
-				
+				var_dump($p);
 				if(isset($p['tags'])) {
 					foreach($p['tags']['data'] as $q){
 						$name =$name . $q['name'].' & ';
