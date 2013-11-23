7e47dec9eae22234fa254ffd9ef90357fb10faf4
ravenjohn <rjlagrimas08@gmail.com>
2013-11-24 05:41:34 +0800

    Try fix search.php

--- application/controller/search.php
+++ application/controller/search.php
@@ -6,9 +6,6 @@ class Search extends Kiel_Controller
 		$this->required_fields($required,$this->get_args);
 		$request = $this->get_args['source'];
 		$searchString = $this->get_args['query'];
-		$id = $this->get_args['id'];
-		$request = urldecode($this->get_args['source']);
-		$searchString = urldecode($this->get_args['query']);
 
 		$ret = array();
 		$count=0;
@@ -39,7 +36,6 @@ class Search extends Kiel_Controller
 					)
 					
 				){
-
 				array_push($fb, array(
 					'place' => '',
 					'sender' => strstr($p['name'], 'Help', true) . ' & '.$name,
@@ -50,10 +46,11 @@ class Search extends Kiel_Controller
 				));
 				}
 		}
-		
-		$ret['fb'] = $fb;
-		}
 
+		$ret['fb'] = $fb;
+
+
+		}
 		$url = 'https://www.google.org/personfinder/2013-yolanda/api/search?key=smo7n6_B3sgRMD9Y&q='.urlencode($searchString);
 		$response = file_get_contents($url);
 		if($response){
@@ -69,85 +66,18 @@ class Search extends Kiel_Controller
 				foreach($p['note'] as $g)
 					$mess = $g['status'] ."\n" .$g['text'];
 			}
-			$matches=array();
-			preg_match('(+63|0|)9(05|06|15|16|17|26|27|35|36|37|94|96|97)[0-7]{7,7}',$p['description'], $matches);
-			
+
+
 			array_push($google, array(
 				'place' => "".$p['home_street'].' : '.$p['home_city']	.' : '. $p['home_state'],
 				'sender' => "".$p['full_name'] .' ' .$p['alternate_names'].' : '.$p['given_name'].$p['family_name'],
-				'number' => "".$matches[0],
+				'number' => '',
 				'message' => "".preg_replace("/(\r\n|\r|\n)/","", $p['description'] . $mess),
 				'from' => "".$p['source_url'],
 			));	
-
-			if(isset($id) && isset($matches[0])){
-				call($id, $searchString, $matches[0]);
-			}
-			
 		}
 		}
-		$data['google'] = $google;
 
-
-		
-		$dswd = array();
-		$url = 'http://cors.io/spreadsheets.google.com/feeds/list/0ApSfq4LnrdaRdHBTSllLTVBaSW9UTjlobUZCNXRNN1E/od6/public/values?alt=json&q='.urlencode($searchString);
-		$response = @file_get_contents($url);
-		if($response){
-			$array = json_decode($response, true);
-			
-			if(isset($array['feed']['entry'])){
-				$temp = $array['feed']['entry'];
-				foreach($temp as $p){
-
-					if(strpos(strtolower($p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t']), strtolower($searchString))!== false)
-					if(!isset($tmp[$p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t']])){
-						array_push($dswd, array(
-							'place' => '',
-							'sender' =>	$p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t'],
-							'number' => '',
-							'message' => 'Survivor',
-							'from' => $p['id']['$t'],
-						));
-						$tmp[$p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t']] = true;
-					}
-
-					if(strpos(strtolower($p['gsx$firstname_2']['$t'].' '.$p['gsx$lastname_2']['$t']), strtolower($searchString))!== false)
-					if(!isset($tmp[$p['gsx$firstname_2']['$t'].' '.$p['gsx$lastname_2']['$t']])){
-					array_push($dswd, array(
-						'place' => '',
-						'sender' =>	$p['gsx$firstname_2']['$t'].' '.$p['gsx$lastname_2']['$t'],
-						'number' => '',
-						'message' => 'Survivor',
-						'from' => $p['id']['$t'],
-					));
-					$tmp[$p['gsx$firstname_2']['$t'].' '.$p['gsx$lastname_2']['$t']] = true;
-					}
-
-					if(strpos(strtolower($p['gsx$firstname_3']['$t'].' '.$p['gsx$lastname_3']['$t']), strtolower($searchString))!== false)
-					if(!isset($tmp[$p['gsx$firstname_3']['$t'].' '.$p['gsx$lastname_3']['$t']])){
-					array_push($dswd, array(
-						'place' => '',
-						'sender' =>	$p['gsx$firstname_3']['$t'].' '.$p['gsx$lastname_3']['$t'],
-						'number' => '',
-						'message' => 'Survivor',
-						'from' => $p['id']['$t'],
-					));
-					$tmp[$p['gsx$firstname_3']['$t'].' '.$p['gsx$lastname_3']['$t']] = true;
-					}
-
-					if(strpos(strtolower($p['gsx$firstname_4']['$t'].' '.$p['gsx$lastname_4']['$t']), strtolower($searchString))!== false)
-					if(!isset($tmp[$p['gsx$firstname_4']['$t'].' '.$p['gsx$lastname_4']['$t']])){
-					array_push($dswd, array(
-						'place' => '',
-						'sender' =>	$p['gsx$firstname_4']['$t'].' '.$p['gsx$lastname_4']['$t'],
-						'number' => '',
-						'message' => 'Survivor',
-						'from' => $p['id']['$t'],
-					));
-					$tmp[$p['gsx$firstname_4']['$t'].' '.$p['gsx$lastname_4']['$t']] = true;
-					}
-				}
 		
 		$ret['google'] = $google;
 
@@ -205,6 +135,9 @@ class Search extends Kiel_Controller
 				'from' => $p['id']['$t'],
 			));
 			$tmp[$p['gsx$firstname_4']['$t'].' '.$p['gsx$lastname_4']['$t']] = true;
+			}
+		}
+
 		}
 
 
@@ -218,8 +151,6 @@ class Search extends Kiel_Controller
 		if($array['data']['result_count'] > 0){
 		$data = $array['data']['result'];
 		foreach($data as $p){
-			$matches=array();
-			preg_match('(+63|0|)9(05|06|15|16|17|26|27|35|36|37|94|96|97)[0-7]{7,7}',$p['sender_number'], $matches);
 			array_push($relief, array(
 				'id' => $p['id'],
 				'place' => urldecode($p['place_tag']),
@@ -228,9 +159,6 @@ class Search extends Kiel_Controller
 				'message' => urldecode($p['message']),
 				'from' => $url,
 			));
-			if(isset($id) && isset($matches[0])){
-				call($id, $searchString, $matches[0]);
-			}
 		}}
 		}
 		$ret['relief'] = $relief;
@@ -243,8 +171,6 @@ class Search extends Kiel_Controller
 		foreach($data as $p){
 			if(preg_match(strtolower('/('.strtolower(str_ireplace(' ','|',$p['location'].' '.$p['name'].' '.$p['message'] )). ')/'), strtolower(urldecode($searchString))))
 			if(strpos(strtolower($p['location'].$p['name'].$p['message']), strtolower($searchString))) {
-			$matches=array();
-			preg_match('(+63|0|)9(05|06|15|16|17|26|27|35|36|37|94|96|97)[0-7]{7,7}',$p['phone'], $matches);
 				array_push($bangon, array(
 					'id'	=> $p['id'],
 					'place' => $p['location'],
@@ -253,11 +179,8 @@ class Search extends Kiel_Controller
 					'message' => $p['message'],
 					'from' => $url,
 				));
-
-			if(isset($id) && isset($matches[0])){
-				call($id, $searchString, $matches[0]);
 			}
-			}
+		}
 		}
 	
 		$ret['bangon'] = $bangon;
@@ -268,14 +191,14 @@ class Search extends Kiel_Controller
 
 			// DO MOBILE HERE
 			$this->response(array('status'=>'Success','data'=>$arr),200);
-		} else if($request == 'subscribe'){	//return 5 E
-			$arr = array_slice($ret,0,5);
+		} else if($request == 'subscibe'){	//return 5 E
+			$arr = array_slice(array_merge($fb, $google, $dswd,$relief, $bangon),0,5);
 			$arr['count'] = count($arr);
 
 			// DO MOBILE HERE
 			$this->response(array('status'=>'Success','data'=>$arr),200);
 		} else {
-			$this->response(array('status'=>'Success','data'=>$data),200);
+			$this->response(array('status'=>'Success','data'=>$ret),200);
 		}
 
 	}
