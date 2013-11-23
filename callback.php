ab663aa3cc0a4a04b0efa7200624e39e7933588e
adin234 <adinbautista@gmail.com>
2013-11-23 05:44:25 -0800

    callback

---
+++ callback.php
@@ -0,0 +1,16 @@
+<?php
+	$insertClause = 'INSERT INTO %s VALUES(%s)';
+	$table = 'users';
+
+	if($_REQUEST['code']) {
+		$response = $auth->getAccessToken($_REQUEST['code']);
+	} else if($_REQUEST['access_token']) {
+		$response = $_REQUEST;
+	}
+
+	if(isset($response['access_token'])) {
+		$link = mysqli_connect("localhost","root","P@ssw0rd","kumusta") or die("Error " . mysqli_error($link));
+		$data = array('NULL', $response['subscriber_number'], $response['access_token'], 'NULL', 'NULL');
+		$query = sprintf($insertClause, $table, implode(',', $data));
+	}
+?>
