2238dd83dfcfe20608626a1b40d21711e0551498
pprmint <neclarin@stratpoint.com>
2013-11-24 01:37:21 +0800

    added search

--- application/controller/search.php
+++ application/controller/search.php
@@ -2,30 +2,43 @@
 class Search extends Kiel_Controller
 {
 	public function index_get(){
-		$required = array('query');
+		$required = array('query','source');
 		$this->required_fields($required,$this->get_args);
+		$request = $this->get_args['source'];
 		$searchString = $this->get_args['query'];
 
 
 		$ret = array();
-		$url = 'http://reliefboard.com/search?loc=1&name=1&message=1&query='.$searchString.'&offset=0&limit=1000000000';
+		$count=0;
+		$tmp = array();
+
+		$url = 'http://graph.facebook.com/666212400066213/photos?fields=name,tags,link&limit=1000';
 		$response = file_get_contents($url);
 		if($response){
-		$json = stripslashes($response);
-		$array = json_decode($json, true);
-		if($array['data']['result_count'] > 0){
-		$data = $array['data']['result'];
-		foreach($data as $p){
-			array_push($ret, array(
-				'place' => $p['place_tag'],
-				'sender' => $p['sender'],
-				'number' => $p['sender_number'],
-				'message' => $p['message'],
-				'from' => $url
-			));
-		}}
+		$array = json_decode($response, true);
+		foreach($array['data'] as $p){
+				$name = "";
+				
+				if(isset($p['tags'])) {
+					foreach($p['tags']['data'] as $q){
+						$name =$name . $q['name'].' & ';
+					}
+				}
+				$p['name'] = preg_replace("/  /", " ", preg_replace("/(\r\n|\r|\n)/", " ", $p['name'])) .' '.$name;
+				if(strpos(strtolower($p['name']), strtolower($searchString)) !== false){
+				array_push($ret, array(
+					'place' => '',
+					'sender' => $p['name'],
+					'number' => '',
+					'message' => strstr($p['name'], 'Help'),
+					'from' => $p['link']
+				));
+				}
 		}
-		
+
+		}
+
+
 		$url = 'http://api.bangonph.com/v1/posts';
 		$response = file_get_contents($url);
 		if($response){
@@ -44,7 +57,24 @@ class Search extends Kiel_Controller
 		}
 		}
 
-
+		$url = 'http://reliefboard.com/search?loc=1&name=1&message=1&query='.$searchString.'&offset=0&limit=1000000000';
+		$response = file_get_contents($url);
+		if($response){
+		$json = stripslashes($response);
+		$array = json_decode($json, true);
+		if($array['data']['result_count'] > 0){
+		$data = $array['data']['result'];
+		foreach($data as $p){
+			array_push($ret, array(
+				'place' => $p['place_tag'],
+				'sender' => $p['sender'],
+				'number' => $p['sender_number'],
+				'message' => $p['message'],
+				'from' => $url
+			));
+		}}
+		}
+		
 		$tmp = array();
 		$url = 'http://cors.io/spreadsheets.google.com/feeds/list/0ApSfq4LnrdaRdHBTSllLTVBaSW9UTjlobUZCNXRNN1E/od6/public/values?alt=json&q='.$searchString;
 		$response = file_get_contents($url);
@@ -55,14 +85,14 @@ class Search extends Kiel_Controller
 
 			if(strpos(strtolower($p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t']), strtolower($searchString))!== false)
 			if(!isset($tmp[$p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t']])){
-			array_push($ret, array(
-				'place' => '',
-				'sender' =>	$p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t'],
-				'number' => '',
-				'message' => 'Survivor',
-				'from' => $p['id']['$t']
-			));
-			$tmp[$p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t']] = true;
+				array_push($ret, array(
+					'place' => '',
+					'sender' =>	$p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t'],
+					'number' => '',
+					'message' => 'Survivor',
+					'from' => $p['id']['$t']
+				));
+				$tmp[$p['gsx$firstname']['$t'].' '.$p['gsx$lastname']['$t']] = true;
 			}
 
 			if(strpos(strtolower($p['gsx$firstname_2']['$t'].' '.$p['gsx$lastname_2']['$t']), strtolower($searchString))!== false)
@@ -104,62 +134,44 @@ class Search extends Kiel_Controller
 
 		}
 
-
-
-		$tmp = array();
-
-		$url = 'http://graph.facebook.com/666212400066213/photos?fields=name,tags,link&limit=1000';
-		$response = file_get_contents($url);
-		if($response){
-		$array = json_decode($response, true);
-		foreach($array['data'] as $p){
-				$name = "";
-				
-				if(isset($p['tags'])) {
-					foreach($p['tags']['data'] as $q){
-						$name =$name . $q['name'].' & ';
-					}
-				}
-				$p['name'] = preg_replace("/  /", " ", preg_replace("/(\r\n|\r|\n)/", " ", $p['name'])) . $name;
-				if(strpos(strtolower($p['name']), strtolower($searchString)) !== false)
-				array_push($ret, array(
-					'place' => '',
-					'sender' => $p['name'],
-					'number' => '',
-					'message' => strstr($p['name'], 'Help'),
-					'from' => $p['link']
-				));
-		}
-
-		}
-
-
-
 		$url = 'https://www.google.org/personfinder/2013-yolanda/api/search?key=smo7n6_B3sgRMD9Y&q='.$searchString;
 		$response = file_get_contents($url);
 		if($response){
-			$data = preg_replace("/pfif\:/", "", $response);
-			$xml = simplexml_load_string($data);
-			$json = json_encode($xml);
-			$array = json_decode($json,TRUE);
-			
-			foreach($array['person'] as $p){
-				$mess = "";
-				if(isset($p['note'])){
-					foreach($p['note'] as $g)
-						$mess = $g['status'] ."\n" .$g['text'];
-				}
-				array_push($ret, array(
-					'place' => $p['home_street']. ' : '. $p['home_state'],
-					'sender' => $p['full_name'] .' ' .$p['alternate_names'],
-					'number' => '',
-					'message' => preg_replace("/(\r\n|\r|\n)/","", $p['description'] . $mess),
-					'from' => $p['source_url']
-				));	
-			}
-		}
+		$data = preg_replace("/pfif\:/", "", $response);
+		$xml = simplexml_load_string($data);
+		$json = json_encode($xml);
+		$array = json_decode($json,TRUE);
 		
-		$this->response(array('status'=>'Success','data'=>$ret),200);
+		foreach($array['person'] as $p){
+			$mess = "";
+			if(isset($p['note'])){
+				foreach($p['note'] as $g)
+					$mess = $g['status'] ."\n" .$g['text'];
+			}
+			array_push($ret, array(
+				'place' => $p['home_street']. ' : '. $p['home_state'],
+				'sender' => $p['full_name'] .' ' .$p['alternate_names'],
+				'number' => '',
+				'message' => preg_replace("/(\r\n|\r|\n)/","", $p['description'] . $mess),
+				'from' => $p['source_url']
+			));	
+		}
+		}
+		if($request == 'mobile'){			// return 3 E
+			$arr = array_slice($ret,0,3);
+			$arr['count'] = count($arr);
+
+			// DO MOBILE HERE
+			$this->response(array('status'=>'Success','data'=>$arr),200);
+		} else if($request == 'subscibe'){	//return 5 E
+			$arr = array_slice($ret,0,5);
+			$arr['count'] = count($arr);
+
+			// DO MOBILE HERE
+			$this->response(array('status'=>'Success','data'=>$arr),200);
+		} else {
+			$this->response(array('status'=>'Success','data'=>$ret),200);
+		}
 
 	}
 }
