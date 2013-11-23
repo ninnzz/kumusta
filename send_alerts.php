aa394c8c230db54d8ad5f6a932f9ff69019d357d
craeden <wildtron10@gmail.com>
2013-11-24 04:33:36 +0800

    automated alerts

---
+++ send_alerts.php
@@ -0,0 +1,7 @@
+<?php
+	$url = "http://ec2-184-169-205-217.us-west-1.compute.amazonaws.com/cron_subscribe?app_id=90an729m.kumusta.web";
+	$json = file_get_contents($url);
+	$array = $json_decode($json, true);
+	var_dump($array);
+
+?>
