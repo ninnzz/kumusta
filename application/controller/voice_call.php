474529ef5cda785bc180513666e303389a975aea
pprmint <neclarin@stratpoint.com>
2013-11-23 20:40:38 +0800

    fix merge

--- application/controller/voice_call.php
+++ application/controller/voice_call.php
@@ -6,18 +6,6 @@ class Voice_call extends Kiel_Controller
 	{
 		require('tropo_voice/tropo.class.php');
 
-		try {
-		  // If there is not a session object in the POST body,
-		  // then this isn't a new session. Tropo will throw
-		  // an exception, so check for that.
-		  $session = new Session();
-		} catch (TropoException $e) {
-		  // This is a normal case, so we don't really need to 
-		  // do anything if we catch this.
-		}
-
-		$caller = $session->getFrom();
-
 		$tropo = new Tropo();
 		// $caller now has a hash containing the keys: id, name, channel, and network
 		
