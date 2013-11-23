e45788bb0d513a895f8b952d9ec334519fe2074c
adin234 <adinbautista@gmail.com>
2013-11-23 05:50:45 -0800

    asdf

--- callback.php
+++ callback.php
@@ -8,8 +8,11 @@
 		$response = $_REQUEST;
 	}
 
+
+	$link = mysqli_connect("localhost","root","P@ssw0rd","kumusta") or die("Error " . mysqli_error($link));
+	print_r($link);
+
 	if(isset($response['access_token'])) {
-		$link = mysqli_connect("localhost","root","P@ssw0rd","kumusta") or die("Error " . mysqli_error($link));
 		$data = array('NULL', $response['subscriber_number'], $response['access_token'], 'NULL', 'NULL');
 		$query = sprintf($insertClause, $table, implode(',', $data));
 	}
