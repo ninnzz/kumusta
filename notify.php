7feb2bed185c3d1bfe2659bcfe8a12f4d191212b
adin234 <adinbautista@gmail.com>
2013-11-23 06:33:03 -0800

    asdf

--- notify.php
+++ notify.php
@@ -22,15 +22,10 @@ if($message) {
 			continue;	
 		}
 
-
 		$link = mysqli_connect("localhost","root","P@ssw0rd","kumusta") or die("Error " . mysqli_error($link));
-		echo 'SELECT * FROM users WHERE phoneNumber = \''.str_replace('tel:+63', '', $item['senderAddress']).'\' LIMIT 1;';
-		$user = $link->query('SELECT * FROM users WHERE phoneNumber = \''.str_replace('tel:+63', '', $item['senderAddress']).'\' LIMIT 1;');
-
-		print_r($user);
-
-		$user = is_array($user) ? $user[0] : NULL;
-
+		$result = $link->query('SELECT * FROM users WHERE phoneNumber = \''.str_replace('tel:+63', '', $item['senderAddress']).'\' LIMIT 1;');
+		$user = $result->fetch_row();
+		
 		echo "the user: "; print_r($user);
 
 		if($user) {
